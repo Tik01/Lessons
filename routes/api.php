a1b5d9a09b2f56ae3ac4a90d0ad657f1902fbac5
Tik01 <sarukhanyantik@gmail.com>
2023-11-02 14:45:17 +0400

    Add api route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/posts',function (){
+    return response()->json([
+        'posts'=>[
+            [
+            'title'=>'POST ONE'
+            ],
+            [
+            'title'=>'POST TEN'
+        ]
+        ]
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
