b737cc9c88a54647b6df1392ccbe71b5e3f2f3c1
Tik01 <sarukhanyantik@gmail.com>
2023-11-03 23:17:35 +0400

    Add ListingController,Tag filter and search filter

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './resources/views/listings/index.blade.php',
+      './resources/views/components/layout.blade.php',
+      './resources/views/partials/search.blade.php'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
